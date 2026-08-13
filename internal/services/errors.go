7822ac6fe8597f31e14284da3bab1041d1fa8673
Lê Trung Hiếu <warmdevofficial@gmail.com>
2026-08-13 23:01:40 +0700

    feat(error): add error user context not found

--- internal/services/errors.go
+++ internal/services/errors.go
@@ -3,14 +3,23 @@ package services
 import "errors"
 
 var (
-	ErrInvalidInput       = errors.New("invalid input")
+	// auth
 	ErrInvalidCredentials = errors.New("invalid credentials")
 	ErrUserNotFound       = errors.New("user not found")
 	ErrEmailTaken         = errors.New("email already registered")
 	ErrUsernameTaken      = errors.New("username already registered")
-	ErrInvalidToken       = errors.New("invalid token")
-	ErrTokenExpired       = errors.New("token expired")
-	ErrTokenNotExists     = errors.New("token is not exists")
-	ErrTokenRevoked       = errors.New("token has been revoked")
-	ErrInternalServer     = errors.New("internal server error")
+	ErrContextNotFound    = errors.New("user context not found")
+
+	// token
+	ErrInvalidToken   = errors.New("invalid token")
+	ErrTokenExpired   = errors.New("token expired")
+	ErrTokenNotExists = errors.New("token is not exists")
+	ErrTokenRevoked   = errors.New("token has been revoked")
+
+	// server
+	ErrInternalServer = errors.New("internal server error")
+	ErrInvalidInput   = errors.New("invalid input")
+
+	// Workspace
+	ErrWorkspaceNotFound = errors.New("workspace not found")
 )
